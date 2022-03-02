6182ab68d30b8182a257fa6cb7ad55e00b3d4cd8
wdev733 <wdev733@gmail.com>
2022-03-02 10:30:59 -0500

    navigate to terra station wallet installation page if not installed

--- components/Modal/ModalSelectWallet.tsx
+++ components/Modal/ModalSelectWallet.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { isBrowser } from 'react-device-detect';
 
 const ModalSelectWallet = ({ onChooseTerraWallet, onChooseWalletConnect }) => {
   // const [val, setVal] = useState("");
@@ -7,13 +8,15 @@ const ModalSelectWallet = ({ onChooseTerraWallet, onChooseWalletConnect }) => {
     <div className="modal-select-wallet">
       <span className="modal-view-title">SELECT A WALLET</span>
       <div className="modal-choose-wallet-list">
-        <div className="wallet-choose-item" onClick={(e) => onChooseTerraWallet()}>
-          <img src="/assets/terra-station.png" style={{ width: 50, height: 51 }} />
-          <div className="wallet-choose-item-text">
-            <span className="wallet-choose-item-name">Terra Station</span>
-            <span className="wallet-choose-item-sub">Connect with Terra Station</span>
+        {isBrowser && (
+          <div className="wallet-choose-item" onClick={(e) => onChooseTerraWallet()}>
+            <img src="/assets/terra-station.png" style={{ width: 50, height: 51 }} />
+            <div className="wallet-choose-item-text">
+              <span className="wallet-choose-item-name">Terra Station</span>
+              <span className="wallet-choose-item-sub">Connect with Terra Station</span>
+            </div>
           </div>
-        </div>
+        )}
 
         <div className="wallet-choose-item" onClick={(e) => onChooseWalletConnect()}>
           <img src="/assets/wallet-connect.svg" style={{ width: 59, height: 36 }} />
