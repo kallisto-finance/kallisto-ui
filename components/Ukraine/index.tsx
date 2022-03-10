301b8a03fc17f132957a75082b2467d42dd803fe
wdev733 <wdev733@gmail.com>
2022-03-10 12:49:40 -0500

    update ukraine banner

--- components/Ukraine/index.tsx
+++ components/Ukraine/index.tsx
@@ -1,19 +1,14 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-
-const faPropIcon = faTimes as IconProp;
+import React from "react";
 
 const Ukraine = () => (
   <div className="ukraine-container-wrapper">
     <div className="ukraine-container">
       <img className="ukraine-icon" src="/assets/ukraine.png" />
       <div className="banner-content">
-        <p className="title">Help to Ukraine!</p>
+        <p className="title">Let’s support Ukraine!</p>
         <p className="content">
-          Kallisto contributes <span className="donate">$50</span> for Ukraine
-          for every $10.000 deposited that is made into the Liquidation Pool.
+          Kallisto donates <span className="donate">$50</span> to Ukraine for
+          every $10.000 deposited into the Liquidation Pool.
         </p>
       </div>
     </div>
