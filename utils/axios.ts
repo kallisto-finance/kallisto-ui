42b8b3039d32e9cf121152a7c311213dffd26717
taariq <taariq.lewis@gmail.com>
2022-04-01 11:34:26 -0700

    update the node endpoint

--- utils/axios.ts
+++ utils/axios.ts
@@ -14,7 +14,7 @@ const getGasPrices = () =>
   axiosClient().get("https://bombay-fcd.terra.dev/v1/txs/gas_prices");
 
 const getTxHistories = async (address, offset = 0, limit = 100) => {
-  const res = await fetch(`https://fcd.terra.dev/v1/txs?offset=${offset}&limit=${limit}&account=${address}`);
+  const res = await fetch(`https://twilight-dark-meadow.terra-mainnet.quiknode.pro/07438b8036e1d218249db16c1131f4b533e6a4cb/v1/txs?offset=${offset}&limit=${limit}&account=${address}`);
   const json = await res.json();
 
   return json;
