16579fc7ac0664a7ffa1e9b246776cd99bca6d8b
Vera Behr <vera.behr@gmail.com>
2022-04-07 15:48:43 -0400

    Update Ukraine donation wallet address

--- pages/ukraine.tsx
+++ pages/ukraine.tsx
@@ -70,11 +70,11 @@ const Ukraine = () => {
                 <b>Every Wednesday</b> the Kallisto team will deposit the
                 collected to Angel Protocol in UST from the wallet address<br/>
                 <a
-                  href="https://finder.terra.money/mainnet/address/terra16d52wwt6t79x3sd35rargp3y5es90m64t0fkzk"
+                  href="https://finder.terra.money/mainnet/address/terra1zsm74pnpr508jy02zzxwhrc2gwg2tn87w9fgn9"
                   target="_blank"
                   style={{ color: "#efce2b" }}
                 >
-                  terra16d52wwt6t79x3sd35rargp3y5es90m64t0fkzk
+                  terra1zsm74pnpr508jy02zzxwhrc2gwg2tn87w9fgn9
                 </a>
               </p>
             </li>
