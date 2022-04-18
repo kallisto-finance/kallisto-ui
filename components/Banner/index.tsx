9e6a17a5b8a11f91438948fb2c181790240518e3
verabehr <vera@volume.finance>
2022-04-18 16:53:28 -0400

    Update banner message

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -11,7 +11,9 @@ const AttentionBanner = () => (
   <div className="attention-banner-container">
     <img className="attention-banner-icon" src="/assets/danger.png" />
     <span className="attention-banner-text">
-      Attention! Kallisto smart contracts are currently paused while we make some improvements. Check back soon!
+      Attention! Kallisto smart contracts are not yet audited and are regularly
+      updated for improvements. Withdrawals from deprecated contracts are always
+      allowed. Use at your own risk
     </span>
   </div>
 );
