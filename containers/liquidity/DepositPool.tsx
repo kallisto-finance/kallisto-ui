2d0bf1ee8b6d674dc3bf041771d8409533f2d300
wdev733 <wdev733@gmail.com>
2022-04-25 11:28:23 -0400

    fix ui issue, withdraw cooldown, decimals

--- containers/liquidity/DepositPool.tsx
+++ containers/liquidity/DepositPool.tsx
@@ -88,14 +88,14 @@ const DepositPool = ({
       <div className="view-container-row">
         <AmountView
           label="7 day Deposits"
-          value={`${formatBalance(volume, 1)} UST`}
+          value={`${formatBalance(volume, 4)} UST`}
           theme={pool.theme}
         />
       </div>
       <div className="view-container-row">
         <AmountView
           label="Liquidity"
-          value={`${formatBalance(pool.totalCap, 1)} UST`}
+          value={`${formatBalance(pool.totalCap, 4)} UST`}
           theme={pool.theme}
         />
       </div>
