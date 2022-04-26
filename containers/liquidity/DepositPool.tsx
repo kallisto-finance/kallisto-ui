6215336106707924ac6d4bfb81f0e29ffdb68785
taariq <taariq.lewis@gmail.com>
2022-04-26 08:15:15 -0700

    20220426 APY update

--- containers/liquidity/DepositPool.tsx
+++ containers/liquidity/DepositPool.tsx
@@ -74,7 +74,7 @@ const DepositPool = ({
         />
       </div>
       <div className="view-container-row">
-        <AmountView label="APY" value="270.3%" highlight={true} />
+        <AmountView label="APY" value="260.8%" highlight={true} />
       </div>
       <div className="view-container-row">
         <AmountView
